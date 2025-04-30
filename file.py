5c986cfe4cdfcfc8ecc2dac8f160581916a866e7
EC2 Default User <ec2-user@ip-172-31-3-215.ap-south-1.compute.internal>
2025-04-30 09:47:23 +0000

    1st by dev3

--- file.py
+++ file.py
@@ -1,7 +1,7 @@
 import boto5
 
 # Initialize EC2 client (region can be changed as needed)
-ec2 = boto3.client('ec2', region_name='us-west-1')
+ec2 = boto5.client('ec2', region_name='us-north-1')
 
 # Launch the instance
 response = ec2.run_instances(
